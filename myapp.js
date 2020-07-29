7f2729dcf013802a22a52e58e807f319cca5e962
Aaron_H <aaron.hen404@outlook.com>
2020-07-29 12:05:41 -0400

    Unknown changes on work PC. Backing up in case there are good changes I want to save.

--- myapp.js
+++ myapp.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+// const myPort = 4096;
 const myPort = 3000;
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
